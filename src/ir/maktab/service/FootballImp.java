18f78b578ffd1e048b9138ea48ba51391e2ea3b8
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-09-11 23:40:50 +0430

    Add team in FootballImp

---
+++ src/ir/maktab/service/FootballImp.java
@@ -0,0 +1,16 @@
+package ir.maktab.service;
+
+import ir.maktab.model.entity.Football;
+import ir.maktab.model.repository.FootballRepository;
+
+import java.sql.SQLException;
+
+public class FootballImp implements League{
+
+    private FootballRepository footballRepository = new FootballRepository();
+
+    @Override
+    public int addTeam(Football football) throws SQLException {
+        return footballRepository.insertTeam(football);
+    }
+}
