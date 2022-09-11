f758d1e837b12a26efc316e19b4369ff69020faa
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-09-11 23:40:32 +0430

    Add team in FootballImp

--- src/ir/maktab/service/League.java
+++ src/ir/maktab/service/League.java
@@ -2,13 +2,14 @@ package ir.maktab.service;
 
 import ir.maktab.model.entity.Football;
 
+import java.sql.SQLException;
 import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
 public interface League {
 
-    public int addTeam(Football football) ;
+    public int addTeam(Football football) throws SQLException;
 
     List <Football> football = new ArrayList<>();
 }
