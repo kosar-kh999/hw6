1fb9d24ca21c7ef3c10c3fd0ccebc2ec79c4b292
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-09-11 23:18:47 +0430

    creat insert team

--- src/ir/maktab/model/repository/FootballRepository.java
+++ src/ir/maktab/model/repository/FootballRepository.java
@@ -1,6 +1,25 @@
 package ir.maktab.model.repository;
 
+import ir.maktab.config.ApplicationConstant;
+import ir.maktab.model.entity.Football;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class FootballRepository {
 
+    public int insertTeam(Football football) throws SQLException {
 
+        String sql = "insert into football_tbl  (name,location,wins,draws,defeats,points,numberOfPlayed)values " +
+                " (?,?,?,?,?,?,?)";
+        PreparedStatement preparedStatement = ApplicationConstant.getConnection().prepareStatement(sql);
+        preparedStatement.setString(1,football.getName() );
+        preparedStatement.setString(2,football.getLocation());
+        preparedStatement.setInt(3,football.getWins() );
+        preparedStatement.setInt(4,football.getDraws() );
+        preparedStatement.setInt(5,football.getDefeats() );
+        preparedStatement.setInt(6, football.getPoints());
+        preparedStatement.setInt(7, football.getNumberOfPlayed());
+        return preparedStatement.executeUpdate();
+    }
 }
