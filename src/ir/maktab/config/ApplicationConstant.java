0fbb1429fe0ec7531a10dda7eaf2b9ed29c3394b
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-09-11 19:21:13 +0430

    creat application constant

--- src/ir/maktab/config/ApplicationConstant.java
+++ src/ir/maktab/config/ApplicationConstant.java
@@ -1,4 +1,11 @@
 package ir.maktab.config;
 
+import java.sql.Connection;
+
 public class ApplicationConstant {
+    private static Connection connection = new DatabaseConnection().getConnection();
+
+    public static Connection getConnection() {
+        return connection;
+    }
 }
