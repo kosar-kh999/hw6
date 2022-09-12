304c6c37567a0631f0af3d513ce06216f8d2a96d
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-09-12 23:38:43 +0430

    creat showInformationTeam method

--- src/ir/maktab/service/League.java
+++ src/ir/maktab/service/League.java
@@ -3,14 +3,14 @@ package ir.maktab.service;
 import ir.maktab.model.entity.Football;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+
 
 public interface League {
 
      int addTeam(Football football) throws SQLException;
 
-    List <Football> football = new ArrayList<>();
-
      void deleteTeam(int points) throws SQLException;
+
+    public List<Football> showInformationTeam() throws SQLException;
 }
