1452148b35c835305d6e57d4cc8c6c53ea4d5eb8
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-09-14 00:57:22 +0430

    creat volleyball class

---
+++ src/ir/maktab/model/entity/VolleyBall.java
@@ -0,0 +1,8 @@
+package ir.maktab.model.entity;
+
+public class VolleyBall extends SportClub {
+
+    public VolleyBall(String name, String location, int wins, int defeats, int points, int numberOfPlayed) {
+        super(name, location, wins, defeats, points, numberOfPlayed);
+    }
+}
